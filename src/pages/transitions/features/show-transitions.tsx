8c30df19675e908103114f67883ad7032bce1ab8
Desmond Tuiyot <dtuiyot@gmail.com>
2024-02-04 16:01:39 -0700

    feat: add ability to delete transitions

--- src/pages/transitions/features/show-transitions.tsx
+++ src/pages/transitions/features/show-transitions.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
-import { List, Tag } from 'antd'
+import { Button, List, Tag } from 'antd'
+import { DeleteOutlined } from '@ant-design/icons'
 
 import useGetTransitions from '../services/get-transitions.api'
+import useRemoveTransition from '../services/remove-transition.api'
+
 import styles from './show-transitions.module.css'
 
 /**
  * Renders the list of transitions
  */
 const ShowTransitions: React.FC = () => {
-  const { data, loading, error } = useGetTransitions()
+  const { data, loading, error, refetch } = useGetTransitions()
+  const removeTransition = useRemoveTransition()
 
   if (loading) return <p>Loading transitions...</p>
   if (error) return <p>Couldn't fetch transitions</p>
@@ -16,6 +20,11 @@ const ShowTransitions: React.FC = () => {
   const getTransitionColor = (id: number) => {
     if (id % 2 === 0) return 'blue'
     return 'green'
+  }
+
+  const handleDelete = async (id: number) => {
+    await removeTransition.call(id)
+    await refetch()
   }
 
   return (
@@ -26,7 +35,14 @@ const ShowTransitions: React.FC = () => {
         dataSource={data?.transitions}
         renderItem={(item, index) => (
           <List.Item>
-            <Tag color={getTransitionColor(index)}>{item?.danceMove1?.name}</Tag> - <Tag color={getTransitionColor(index+1)}>{item?.danceMove2?.name}</Tag>
+            <div className={styles.listContent}>
+              <div>
+                <Tag color={getTransitionColor(index)}>{item?.danceMove1?.name}</Tag> - <Tag color={getTransitionColor(index+1)}>{item?.danceMove2?.name}</Tag>
+              </div>
+              <Button type='text' onClick={() => handleDelete(item.id)}>
+                <DeleteOutlined />
+              </Button>
+            </div>
           </List.Item>
         )}
       />
