50877dbd05ee7cbbeddff1200ab24131a6364488
Desmond Tuiyot <dtuiyot@gmail.com>
2024-02-03 23:17:33 -0700

    feat: add layout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { Link } from "react-router-dom"
+import Layout from "./pages/layout"
 
 const App: React.FC = () => {
   return (
-    <div>
-      <h1>Perceuse</h1>
-      <Link to='/dance-move'>Check Out Dance Moves</Link>
-    </div>
+    <Layout />
   )
 }
 
---
+++ src/pages/layout.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import GraphView from './graph-view/features/graph-view'
+import styles from './layout.module.css'
+import { Outlet } from 'react-router-dom'
+
+/**
+ * Component to lay out the graph view and the sidebar
+ * @returns 
+ */
+const Layout: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.sideBar}><Outlet /></div>
+      <div className={styles.container}><GraphView /></div>
+    </div>
+  )
+}
+
+export default Layout
