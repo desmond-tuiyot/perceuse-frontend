c3a0a91956d007498749463aad0cb6aa27cde1ea
Desmond Tuiyot <dtuiyot@gmail.com>
2024-02-04 14:35:45 -0700

    refact: move the DanceMove component to live under `pages/dance-move`
    Also add documentation, and rename the component to `index`

--- src/pages/dance-move/index.tsx
+++ src/pages/dance-move/index.tsx
@@ -1,8 +1,11 @@
-import DanceMovesView from './dance-move/features/show-dance-moves'
-import AddDanceMove from './dance-move/features/add-dance-move'
+import DanceMovesView from './features/show-dance-moves'
+import AddDanceMove from './features/add-dance-move'
 
 import styles from './dance-move.module.css'
 
+/**
+ * Lays out the dance moves list view & the add dance move form
+ */
 const DanceMove: React.FC = () => {
   return (
     <div className={styles.container}>
